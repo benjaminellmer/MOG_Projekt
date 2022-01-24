c890cc5523a4949d02c51c2459ee129388b628aa
Paul Pfisterer <paul.pfisterer@gmx.at>
2022-01-24 13:05:29 +0100

    Repaired stage transition, updated menu design

---
+++ Implementation/AllTheWayDown/Assets/Scripts/OpenSettings.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OpenSettings : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        MenuManager.inst.GoToSettings();
+    }
+}
